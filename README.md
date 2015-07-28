360f96a559755731f6931c159b2ab0f739aff0dc
Roger Adams <adamsrog04@gmail.com>
2015-07-28 16:11:08 -0700

    update name of project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bcpla
+# WHIIEC Health IT Educational Listings
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'bcpla',
+    modulePrefix: 'whiiec-listing',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
