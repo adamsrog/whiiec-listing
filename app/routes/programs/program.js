e70d652c984a31aeb8a54473e14ef7148ca19e87
Roger Adams <adamsrog04@gmail.com>
2015-07-29 15:05:14 -0700

    build out individual program view, add breadcrumbs

--- app/routes/programs/program.js
+++ app/routes/programs/program.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function() {
-		return this.store.findAll('program');
+	model: function(params) {
+		return this.store.find('program', params.id);
 	}
 });
