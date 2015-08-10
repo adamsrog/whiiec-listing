a9218b62b464e70557b9669ff33b8a109489abea
Roger Adams <adamsrog04@gmail.com>
2015-08-10 10:20:34 -0700

    redirect to programs.index if there is no model.content

--- app/routes/programs/program.js
+++ app/routes/programs/program.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
-		return this.store.find('program', params.id);
+		var model = this.store.find('program', params.id);
+		if (!model.content) { 
+			this.transitionTo('programs.index');
+		}
+		return model;
 	}
 });
