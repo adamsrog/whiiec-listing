5bac0afe07bba7f034d36754bbecb3cc8d69f85c
Roger Adams <adamsrog04@gmail.com>
2015-08-10 10:37:16 -0700

    add last_updated field to program model

--- app/routes/programs/program.js
+++ app/routes/programs/program.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(params) {
 		var model = this.store.find('program', params.id);
-		if (!model.content) { 
-			this.transitionTo('programs.index');
-		}
+		// if (!model.content) { 
+		// 	this.transitionTo('programs.index');
+		// }
 		return model;
 	}
 });
