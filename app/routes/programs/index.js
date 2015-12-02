56745bdfe3cdf453f80b120335e2311d8394e7e1
Roger Adams <adamsrog04@gmail.com>
2015-12-01 22:16:29 -0800

    add clear school filter selections

--- app/routes/programs/index.js
+++ app/routes/programs/index.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+	resetController: function(controller, isExiting, transition) {
+		if (isExiting) {
+			console.log('exiting!', isExiting);
+		}
+	},
+
 	actions: {
 		clearFilterQuery: function() {
 			this.controllerFor('programs/index').set('filterQuery', '');
