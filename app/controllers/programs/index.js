77796c37ec4e5b55238924a6f5bd626af7a50f81
Roger Adams <adamsrog04@gmail.com>
2015-12-07 12:38:08 -0800

    fix ember deprecation notices

--- app/controllers/programs/index.js
+++ app/controllers/programs/index.js
@@ -5,53 +5,63 @@ export default Ember.Controller.extend({
 	session: Ember.inject.service(),
 	filterQuery: '',
 
-	sortedPrograms: Ember.computed('model.content', 'filterQuery', 'selectedSchools.length', 'model.content.length', function() {
-		var programs = this.get('model');
-		var query = this.get('filterQuery');
-		var schools = this.get('selectedSchools');
-		this.set('page', 1);
+	sortedPrograms: Ember.computed('model.content', 'filterQuery', 'selectedSchools.length', 'model.content.length', {
+		get: function() {
+			var programs = this.get('model');
+			var query = this.get('filterQuery');
+			var schools = this.get('selectedSchools');
+			this.set('page', 1);
 
-		// apply search query
-		if (query) {
-			programs = programs.filter(function(program) {
-				var fullname = program.get('institution') + ' ' + program.get('program');
-				if (fullname.toLowerCase().indexOf(query.toLowerCase()) > -1) { return true; }
-			});
+			// apply search query
+			if (query) {
+				programs = programs.filter(function(program) {
+					var fullname = program.get('institution') + ' ' + program.get('program');
+					if (fullname.toLowerCase().indexOf(query.toLowerCase()) > -1) { return true; }
+				});
+			}
+
+			// apply school filter
+			if (schools.length > 0) {
+				// create array from all schools names to compare against
+				var schoolNames = [];
+				schools.forEach(function(school) {
+					schoolNames.push(school.name);
+				});
+
+				programs = programs.filter(function(program) {
+					if (schoolNames.contains(program.get('institution'))) {	return true; }
+				});
+			}
+
+			return programs;
+		},
+		set: function(key, value) {
+			return value;
 		}
-
-		// apply school filter
-		if (schools.length > 0) {
-			// create array from all schools names to compare against
-			var schoolNames = [];
-			schools.forEach(function(school) {
-				schoolNames.push(school.name);
-			});
-
-			programs = programs.filter(function(program) {
-				if (schoolNames.contains(program.get('institution'))) {	return true; }
-			});
-		}
-
-		return programs;
 	}),
 
-	schoolList: Ember.computed('model.content.length', function() {
-		var schools = [];
-		var schoolObjects = [];
-		var programs = this.get('model');
-		
-		// create an array containing school names and controller properties for the checkboxes
-		programs.forEach(function(program) {
-			var institution = {
-				name: program.get('institution'),
-				isSelected: false
-			};
-			if (schools.indexOf(institution.name) === -1) {
-				schools.push(institution.name);
-				schoolObjects.push(institution);
-			}
-		});
-		return schoolObjects.sortBy('name');
+	schoolList: Ember.computed('model.content.length', {
+		get: function() {
+			var schools = [];
+			var schoolObjects = [];
+			var programs = this.get('model');
+			
+			// create an array containing school names and controller properties for the checkboxes
+			programs.forEach(function(program) {
+				var institution = {
+					name: program.get('institution'),
+					isSelected: false
+				};
+				if (schools.indexOf(institution.name) === -1) {
+					schools.push(institution.name);
+					schoolObjects.push(institution);
+				}
+			});
+			return schoolObjects.sortBy('name');
+		},
+		set: function(key, value) {
+			return value;
+		}
 	}),
 
 	selectedSchools: [],
--- app/routes/programs/program.js
+++ app/routes/programs/program.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
 	model: function(params) {
 		return this.store.find('program', params.id);
 	},
-	afterModel: function(program, transition) {
+	afterModel: function(program) {
 		// confirm analyics are loaded and log event
 		if (typeof ga === 'function') {
 			var name = program.get('program');
