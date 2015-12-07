3a34ae954ddf28827e607258e5e2ad839ac0be74
Roger Adams <adamsrog04@gmail.com>
2015-12-06 23:41:07 -0800

    view inventory menu and hero button now reset the filters in the inventory

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	session: Ember.inject.service(),
 	actions: {
+		viewInventory: function() {
+			// reset the selected schools and filter query then transition to inventory
+			this.controllerFor('programs.index').setProperties({
+				selectedSchools: [],
+				filterQuery: ''
+			});
+			this.transitionTo('programs');
+		},
 		logout: function() {
 			this.get('session').invalidate();
 		}
--- app/routes/programs/index.js
+++ app/routes/programs/index.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-	resetController: function(controller, isExiting, transition) {
-		if (isExiting) {
-			console.log('exiting!', isExiting);
-		}
-	},
-
 	actions: {
 		clearFilterQuery: function() {
 			this.controllerFor('programs/index').set('filterQuery', '');
