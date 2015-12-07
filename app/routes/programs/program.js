1bab60163714a84f98c1d3645daac8055698fcc5
Roger Adams <adamsrog04@gmail.com>
2015-12-07 00:19:54 -0800

    program view now triggers google analytic event to track program and school view

--- app/routes/programs/program.js
+++ app/routes/programs/program.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(params) {
 		return this.store.find('program', params.id);
+	},
+	afterModel: function(program, transition) {
+		// confirm analyics are loaded and log event
+		if (typeof ga === 'function') {
+			var name = program.get('program');
+			var institution = program.get('institution');
+			ga('send', 'event', 'Program View', institution + ' - ' + name);
+			ga('send', 'event', 'Institution View', institution);
+		}
 	}
 });
--- config/environment.js
+++ config/environment.js
@@ -35,6 +35,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.googleAnalytics = { webPropertyId: 'UA-70911733-2' };
   }
 
   if (environment === 'test') {
